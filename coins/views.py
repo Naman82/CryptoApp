685dc2a2c1a02247a54c6ae671f548b545abeeaa
Naman82 <b520031@iiit-bh.ac.in>
2022-02-05 22:55:29 +0530

    static website ready

--- coins/views.py
+++ coins/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    data=requests.get(url).json()
+    return render(request,'home.html',{'data':data})
